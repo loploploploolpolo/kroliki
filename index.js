7915878868cdec40669acfb72369cc8086f955c3
loploploploolpolo <127666799+loploploploolpolo@users.noreply.github.com>
2023-03-24 16:40:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+let gameStart = document.querySelector("#gameStart");
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
+
+
+//function for score
+let scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+
+//start Game
+window.addEventListener("keydown", (start) => {
+    //    console.log(start);
+    if (start.code == "Enter") {
+        gameOver.style.display = "none";
+		gameStart.style.display = "none";
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+
+        //score
+        let playerScore = 0;
+        interval = setInterval(scoreCounter, 200);
+    }
+});
+
+
+//jump Your Character
+window.addEventListener("keydown", function(event) {
+    jump();
+});
+
+function jump() {
+    if (dino.classList != "jump"){
+        dino.classList.add("jump")
+    }
+   setTimeout (function(){
+    dino.classList.remove("jump")
+   }, 300)
+}
+
+//'Game Over' if 'Character' hit The 'Block'
+let result = setInterval(() => {
+    let dinoTop = parseInt(getComputedStyle(dino).getPropertyValue("top"));
+    //    console.log("dinoBottom" + dinoBottom);
+
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
+    //    console.log("BlockLeft" + blockLeft);
+
+    if (dinoTop >= 140 && blockLeft >= 0 && blockLeft <= 50) {
+        //        console.log("Game Over");
+
+        gameOver.style.display = "block";
+        block.classList.remove("blockActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
+    }
+}, 10);
